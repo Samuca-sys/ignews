3d1868be57677a6382aafb2c86720c8ad2963841
Samuel Macedo <samuel.macfer@gmail.com>
2021-03-22 10:51:07 -0300

    ci:  config page dinamic title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+import Head from 'next/head';
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
+
     <div className={styles.title}>
-      <h1>Hello World</h1>
+      <Head><title>Início | ig.news</title></Head>
+      <h1>
+
+
+        Hello World
+
+      </h1>
     </div>
   )
 }
