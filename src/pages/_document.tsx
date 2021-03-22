8ddb0ea2ced32fd3b54b41e1573607193ba441ac
Samuel Macedo <samuel.macfer@gmail.com>
2021-03-22 10:34:13 -0300

    ci: create _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>ig.news</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
