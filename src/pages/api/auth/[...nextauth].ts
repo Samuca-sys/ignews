494b47fe2da7f6996bf0cd4c8c953940d0c14fc3
Samuel Macedo <samuel.macfer@gmail.com>
2021-03-23 09:22:39 -0300

    ci: add query to verify duplicate user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,16 +13,45 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+
   callbacks: {
     async signIn(user, acount, profile) {
       const { email } = user
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
-          ));
+          //IF (!(SELECT user_id FROM users 
+          //  WHERE user_id = LOWERCASE(user_by_email)))
+          //-------------------------------------------------
+          //      INSERT INTO users (user_id) VALUES (user_by_email) 
+          //-------------------------------------------------
+          // ELSE IF(SELECT user_id FROM users 
+          //  WHERE user_id = LOWERCASE(user_by_email))
+          //-------------------------------------------------
+          //      (SELECT user_email user_id FROM users 
+          //          WHERE user_id = LOWERCASE(user_by_email))
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
         return true
       }
       catch {
