9786e50328d21d088c059376e4c16b4b059c43b4
Samuel Macedo <samuel.macfer@gmail.com>
2021-03-24 14:14:29 -0300

    ci: add session

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps } from "next"
+import { getSession } from "next-auth/client"
 import Head from "next/head"
-//import { getSession } from "next-auth/client"
 import { RichText } from "prismic-dom"
 import { getPrismicClient } from "../../services/prismic"
 
@@ -34,9 +34,10 @@ export default function Post({ post }: PostProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-  // const session = await getSession({ req })
+  const session = await getSession({ req })
   const { slug } = params
 
+  console.log('SESSION ', session)
   // if(!session) {
 
   // }
